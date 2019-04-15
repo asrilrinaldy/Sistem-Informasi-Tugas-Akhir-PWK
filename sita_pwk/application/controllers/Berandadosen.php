ca61626810d39e7c9bc29f2bc1aba32e256776d3
RodliyatunNichlah15 <RodliyatunNichlah15@users.noreply.github.com>
2019-04-16 05:43:51 +0700

    .

--- sita_pwk/application/controllers/Berandadosen.php
+++ sita_pwk/application/controllers/Berandadosen.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class berandadosen extends CI_Controller {
+class Berandadosen extends CI_Controller {
 
 	public function index()
 	{
--- sita_pwk/application/views/dosen/template.php
+++ sita_pwk/application/views/dosen/template.php
@@ -245,11 +245,12 @@
       <!-- main-panel ends -->
       
       <!-- disini konten -->
-<!--     <?php $this->load->view($konten); ?> -->
+    <?php $this->load->view($konten); ?>
     <!-- /.konten -->
 
     </div>
     <!-- page-body-wrapper ends -->
+  </div>
   <!-- container-scroller -->
 
         <!-- content-wrapper ends -->
@@ -273,11 +274,11 @@
   <!-- Plugin js for this page-->
   <!-- End plugin js for this page-->
   <!-- inject:js -->
-  <script src="<?php base_url() ?>assets/js/off-canvas.js"></script>
-  <script src="<?php base_url() ?>assets/js/misc.js"></script>
+  <script src="<?php base_url() ?>js/off-canvas.js"></script>
+  <script src="<?php base_url() ?>js/misc.js"></script>
   <!-- endinject -->
   <!-- Custom js for this page-->
-  <script src="<?php base_url() ?>assets/js/dashboard.js"></script>
+  <script src="<?php base_url() ?>js/dashboard.js"></script>
   <!-- End custom js for this page-->
 </body>
 
