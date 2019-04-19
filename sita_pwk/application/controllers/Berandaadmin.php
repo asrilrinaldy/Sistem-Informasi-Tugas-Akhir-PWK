28c10edf3c61d70504c85c6e674258e4c5224001
mragil <mragill98@gmail.com>
2019-04-19 20:20:45 +0700

    Fix : Mencegah akses ke controller lain yg buka haknya

--- sita_pwk/application/controllers/Berandaadmin.php
+++ sita_pwk/application/controllers/Berandaadmin.php
@@ -3,6 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class berandaadmin extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		if($this->session->userdata('akses') != "Admin") {
+			if($this->session->userdata('akses') == "Dosen") {
+				$url = base_url('berandadosen');
+				redirect($url);
+			}else{
+				$url = base_url('berandamahasiswa');
+				redirect($url);
+			}
+		}
+	}
+
 	public function index()
 	{
 		$isi['konten'] = 'admin/berandaadmin';
