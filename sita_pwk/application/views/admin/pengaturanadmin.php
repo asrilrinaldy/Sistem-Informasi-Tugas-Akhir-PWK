96ff814491b6d24c3587cf2c3ded230aee95c4a2
RodliyatunNichlah15 <RodliyatunNichlah15@users.noreply.github.com>
2019-04-19 13:54:30 +0700

    update pengaturanadmin

--- sita_pwk/application/views/admin/pengaturanadmin.php
+++ sita_pwk/application/views/admin/pengaturanadmin.php
@@ -1,173 +1,193 @@
-      <!-- partial -->
-      <div class="main-panel">
-        <div class="content-wrapper">
-          <div id="header">
+<!-- partial -->
+<div class="main-panel">
+  <div class="content-wrapper">
+    <div id="header">
             <h1><b>
-        <font color=#308ee0> KELOLA ADMINISTRATOR</font></h1></b>
-        <hr width=100% size=8 NOSHADE class="hr hr-prmary" style="height: 10px; background-color: #308ee0">
+        <font color=#12688D> KELOLA ADMINISTRATOR</font></h1></b>
+        <hr width=100% size=8 NOSHADE class="hr hr-prmary" style="height: 10px; background-color: #12688D">
           </div>
-        </div>
+    <div class="row">
 
-         <!-- tabel mahasiswa -->
-              <div class="card">
-                <div class="card-body">
-                  <div class="content-wrapper">
-                    <a href="<?php echo base_url('tambahadmin')?>" class="btn btn-primary">Tambah +</a><br />
+
+    </div>
+
+   <!-- tabel mahasiswa -->
+
+    <div class="row">
+      <div class="col-lg-12 grid-margin">
+        <div class="card">
+          <div class="card-body">
+             <a href="<?php echo base_url('tambahadmin')?>" style=" background-color: #12688D"><font color="#ffffff">
+              <i class="mdi mdi-plus-circle"></i>
+             Tambah</font></a>
                   <div class="card-title">
-                    <h5><font color="#ffffff" >DATA ADMIN</font></h5>
-                  <div class="table-responsive">
-                    <table class="table table-bordered">
-                      <thead>
-                        <tr>
-                          <th>
-                            #
-                          </th>
-                          <th>
-                            Nama
-                          </th>
-                          <th>
-                            NIM
-                          </th>
-                          <th>
-                            Angkatan
-                          </th>
-                          <th>
-                            Konsentrasi
-                          </th>
-                          <th>
-                            Judul
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td class="font-weight-medium">
-                            1
-                          </td>
-                          <td>
-                            Herman Beck
-                          </td>
-                          <td>
-                            14116072
-                          </td>
-                          <td>
-                            2016
-                          </td>
-                          <td class="text-danger"> semen
-                            <i class="mdi mdi-arrow-down"></i>
-                          </td>
-                          <td>
-                            semen padang
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="font-weight-medium">
-                            2
-                          </td>
-                          <td>
-                            Messsy Adam
-                          </td>
-                          <td>
-                           141160958
-                            </div>
-                          </td>
-                          <td>
-                            2009
-                          </td>
-                          <td class="text-success"> batuan
-                            <i class="mdi mdi-arrow-up"></i>
-                          </td>
-                          <td>
-                            pelapukan
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="font-weight-medium">
-                            3
-                          </td>
-                          <td>
-                            John Richards
-                          </td>
-                          <td>
-                           14117689
-                          </td>
-                          <td>
-                            1998
-                          </td>
-                          <td class="text-danger"> lava
-                            <i class="mdi mdi-arrow-down"></i>
-                          </td>
-                          <td>
-                            gunung
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="font-weight-medium">
-                            4
-                          </td>
-                          <td>
-                            Peter Meggik
-                          </td>
-                          <td>
-                           13456678
-                          </td>
-                          <td>
-                            1944
-                          </td>
-                          <td class="text-danger"> NKRI
-                            <i class="mdi mdi-arrow-down"></i>
-                          </td>
-                          <td>
-                            kemerdekaan
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="font-weight-medium">
-                            5
-                          </td>
-                          <td>
-                            Edward
-                          </td>
-                          <td>
-                           11987767
-                          </td>
-                          <td>
-                            112M
-                          </td>
-                          <td class="text-success"> barbarian
-                            <i class="mdi mdi-arrow-up"></i>
-                          </td>
-                          <td>
-                            Perang romawi
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="font-weight-medium">
-                            6
-                          </td>
-                          <td>
-                            Henry Tom
-                          </td>
-                          <td>
-                           11908876
-                          </td>
-                          <td>
-                            2310
-                          </td>
-                          <td class="text-danger"> neuron
-                            <i class="mdi mdi-arrow-down"></i>
-                          </td>
-                          <td>
-                            sel darah
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
+                    <br>
+                    <center><h3><font color="#ffffff">DATA ADMIN</font>
+                    </h3></center><br>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="jumlah-tampilan" id="kelas_length">Show
+                <label> <select name="kelas_length" aria-controls="kelas" class="form-control input-sm">
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  </select>
+                </label> Entries
               </div>
             </div>
-          </h1></div>
+            <div class="col-sm-6">
+              <div  class="dataTables_filter"> Search
+                <label>
+
+                 <input type="search" class="form-control input-sm" placeholder="" aria-controls="kelas">
+                </label>
+              </div>
+            </div>
           </div>
+
+
+            <div class="table-responsive">
+              <table class="table table-bordered">
+                <thead class="nama-kolom">
+                  <tr>
+                    <th>
+                      No
+                    </th>
+                    <th>
+                      Nama
+                    </th>
+                    <th>
+                      Email
+                    </th>
+                    <th>
+                      Username
+                    </th>
+                    <th>
+                      Telephone
+                    </th>
+                    <th>
+                      Aksi
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td >
+                      1
+                    </td>
+                    <td>
+                      Rodliyatun Nichlah
+                    </td>
+                    <td>
+                      atunnichlah@gmail.com
+                    </td>
+                    <td>
+                      Admin002
+                    </td>
+                    <td>
+                      089631500093
+                    </td>
+                    <td class="aksi">
+                      <a class="nav-link" href="editdata">
+                       <i class="mdi mdi-settings"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="font-weight-medium">
+                      2
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                      </div>
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td class="aksi">
+                      <a class="nav-link" href="editdata">
+                       <i class="menu-icon mdi mdi-settings"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="font-weight-medium">
+                      3
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td class="aksi">
+                      <a class="nav-link" href="editdata">
+                       <i class="mdi mdi-settings"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="font-weight-medium">
+                      4
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+
+                    <td class="aksi">
+
+                        <a class="nav-link" href="editdata">
+                         <i class="mdi mdi-settings"></i>
+                        </a>
+
+                    </td>
+
+                  </tr>
+
+                </tbody>
+              </table>
+            </div>
+
+          <br></br>
+            <div class="row">
+              <div class="col-sm-5">
+                <div class="dataTables_info" id="kelas_info" role="status" aria-live="polite">Showing 1 to 4 of 12 entries
+                </div>
+              </div>
+
+              <div class="col-sm-7">
+
+                  <ul class="tombol-tabel">
+                    <a href="#" class="previous">&laquo; Previous</a>
+                    <a href="#" class="next">Next &raquo;</a>
+                  </ul>
+
+              </div>
+            </div>
+</div></div>
           </div>
-      <!-- main-panel ends -->
+        </div>
+      </div>
+    </div>
+<!-- main-panel ends -->
