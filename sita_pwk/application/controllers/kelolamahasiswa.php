c596df35ed7321405ae902d82b22a58a3ededa90
mragil <mragill98@gmail.com>
2019-04-22 16:19:52 +0700

    Fitur Kelola Mahasiswa (Admin)

--- sita_pwk/application/controllers/kelolamahasiswa.php
+++ sita_pwk/application/controllers/kelolamahasiswa.php
@@ -1,11 +1,32 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class kelolamahasiswa extends CI_Controller {
-
-	public function index()
-	{
-		$isi['konten'] = 'admin/mahasiswa/kelolamahasiswa';
+class Kelolamahasiswa extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+		$this->load->model('mahasiswa_model');
+	}
+	function index(){
+		$isi['konten'] = 'admin/Mahasiswa/kelolamahasiswa';
 		$this->load->view('admin/template', $isi);
 	}
+
+	function mahasiswa_data(){
+		$data=$this->mahasiswa_model->mahasiswa_list();
+		echo json_encode($data);
+	}
+
+	function save(){
+		$data=$this->mahasiswa_model->save_mahasiswa();
+		echo json_encode($data);
+	}
+
+	function update(){
+		$data=$this->mahasiswa_model->update_mahasiswa();
+		echo json_encode($data);
+	}
+
+	function delete(){
+		$data=$this->mahasiswa_model->delete_mahasiswa();
+		echo json_encode($data);
+	}
+
 }
