59e7ddd424ba7ff3094f9078e529bdb4737f3035
RodliyatunNichlah15 <RodliyatunNichlah15@users.noreply.github.com>
2019-04-23 13:02:05 +0700

    update template admin

--- sita_pwk/application/views/admin/template.php
+++ sita_pwk/application/views/admin/template.php
@@ -284,10 +284,7 @@ h3 {
         <footer class="footer">
           <div class="container-fluid clearfix">
             <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2019
-              <a>Kelompok 4</a>. All rights reserved.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
-              <i class="mdi mdi-heart text-danger"></i>
-            </span>
+              <a>Kelompok 6</a> Proyek Perangkat Lunak RA</span>
           </div>
         </footer>
 
