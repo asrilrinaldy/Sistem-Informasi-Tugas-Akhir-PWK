8bde88dfd64240858b65cabf38b2577d5ee5b54e
Febri Dwi Putro <febridwipoethro@gmail.com>
2019-04-27 17:53:58 +0700

    enter

--- sita_pwk/application/models/Login_model.php
+++ sita_pwk/application/models/Login_model.php
@@ -2,36 +2,21 @@
 class Login_model extends CI_Model
 {
 
-<<<<<<< HEAD
   function cek_admin($username, $password)
   {
-    $query = $this->db->query("SELECT * FROM admin WHERE username='$username' AND password=MD5('$password') LIMIT 1");
+    $query = $this->db->query("SELECT * FROM admin WHERE email='$username' AND password=MD5('$password') LIMIT 1");
     return $query;
   }
 
   function cek_dosen($username, $password)
   {
-    $query = $this->db->query("SELECT * FROM dosen WHERE NIP='$username' AND password=MD5('$password') LIMIT 1");
+    $query = $this->db->query("SELECT * FROM dosen WHERE Email='$username' AND password=MD5('$password') LIMIT 1");
     return $query;
   }
 
   function cek_mhs($username, $password)
   {
-    $query = $this->db->query("SELECT * FROM mahasiswa WHERE NIM='$username' AND password=MD5('$password') LIMIT 1");
-=======
-  function cek_admin ($username,$password) {
-    $query = $this->db->query("SELECT * FROM admin WHERE email='$username' AND password=MD5('$password') LIMIT 1");
-    return $query;
-  }
-
-  function cek_dosen ($username,$password) {
-    $query = $this->db->query("SELECT * FROM dosen WHERE Email='$username' AND password=MD5('$password') LIMIT 1");
-    return $query;
-  }
-
-  function cek_mhs ($username,$password) {
     $query = $this->db->query("SELECT * FROM mahasiswa WHERE Email='$username' AND password=MD5('$password') LIMIT 1");
->>>>>>> master
     return $query;
   }
 }
