9f4a1f600af931ced3b1e7aff3390cd76dcd31e4
Asril Rinaldi <asrilrinaldi@gmail.com>
2019-04-30 21:23:54 +0700

    update konsultasi mahasiswa

--- sita_pwk/application/views/mahasiswa/konsultasi.php
+++ sita_pwk/application/views/mahasiswa/konsultasi.php
@@ -9,8 +9,8 @@
             Judul Tugas Akhir
           </h5>
             <div class="biodata-dosen">
-              <br>Pembimbing 1 :</br>
-              <br>Pembimbing 2 :</br>
+              <br><b>Pembimbing 1 : <?php echo $this->session->userdata("ses_pembimbing") ?></b></br>
+              <br><b>Pembimbing 2 : <?php echo $this->session->userdata("ses_pembimbing2") ?></b></br>
             </div>
           </div>
         </div>
@@ -23,40 +23,38 @@
             <h5>
               <i class="menu-icon mdi mdi-paperclip"></i>KONSULTASI
             </h5>
-
-            <div class="biodata-dosen">
-              <br>Berkas File</br>
-              <div class="input-group">
-
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+            <form id="submit" >
+              <div class="biodata-dosen">
+                <br>Berkas File</br>
+                <div class="input-group">
+                  <div class="custom-file">
+                    <input type="file" name="file_konsul" id="file_konsul" enctype="multipart/form-data">
+                  </div>
                 </div>
-                <div class="custom-file">
-                  <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-                  <label class="custom-file-label" for="inputGroupFile01" >Choose file</label>
+                <br>
+                <div>Pembimbing</br>
+                  <label> <select name="dosen" id="dosen" aria-controls="kelas" class="form-control input-sm">
+                    <option value="">No Selected</option>
+                    <?php foreach($dosen as $row):?>
+                    <option value="<?= $row->NIP;?>"><?= $row->Nama;?></option>
+                    <?php endforeach;?>
+                    </select>
+                  </label>
                 </div>
+                <input type="text" id="nip" name="nip">
+                <br>
+                <label>Keterangan</label>
+                <textarea name="keterangan" id="keterangan" class="form-control" rows="8"></textarea><br>
+                <center>
+                    <input class="btn btn-primary" type="submit" value="Kirim" ></input>
+                </center>
               </div>
-              <br>
-              <div class="jumlah-tampilan" id="kelas_length">Pembimbing</br>
-                <label> <select name="kelas_length" aria-controls="kelas" class="form-control input-sm">
-                  <option value="Dosen 1">Dosen 1</option>
-                  <option value="Dosen 2">Dosen 2</option>
-
-                  </select>
-                </label>
-              </div>
-              <br>
-
-              <label>Keterangan</label>
-              <textarea class="form-control" rows="8"></textarea><br>
-              <center>
-                  <input class="btn btn-primary" type="submit" value="Kirim" ></input>
-              </center>
-            </div>
-
+            </form>
           </div>
         </div>
       </div>
+
+
 
       <div class="col-lg-8 grid-margin">
         <div class="card card-statistics">
@@ -66,142 +64,25 @@
               <i class="menu-icon mdi mdi-paperclip"></i>
               Riwayat Konsultasi
             </span><br></br>
-            <div class="row">
-              <div class="col-sm-6">
-                <div class="jumlah-tampilan" id="kelas_length">Show
-                  <label> <select name="kelas_length" aria-controls="kelas" class="form-control input-sm">
-                    <option value="10">10</option>
-                    <option value="25">25</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                    </select>
-                  </label> Entries
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div  class="dataTables_filter"> Search
-                  <label>
-                   <input type="search" class="form-control input-sm" placeholder="" aria-controls="kelas">
-                  </label>
-                </div>
-              </div>
-            </div>
-
             <div class="table-responsive">
-              <table class="table table-bordered">
-                <thead class="nama-kolom">
+              <table id="tabelKonsul" class="table table-striped">
+                <thead>
                   <tr>
-                    <td>
-                      No
-                    </td>
-                    <td>
-                      Tanggal
-                    </td>
-                    <td>
-                      Status
-                    </td>
-                    <td>
-                      File
-                    </td>
-                    <td>
-                      Detail
-                    </td>
-
+                    <th>No</th>
+                    <th>Tanggal</th>
+                    <th>Status</th>
+                    <th>File</th>
+                    <th>Keterangan</th>
+                    <th>Komentar</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <td >
-                      1
-                    </td>
-                    <td>
+                <tbody id="show_data">
 
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-
-                  </tr>
-                  <tr>
-                    <td class="font-weight-medium">
-                      2
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-
-                  </tr>
-                  <tr>
-                    <td class="font-weight-medium">
-                      3
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-
-                  </tr>
-                  <tr>
-                    <td class="font-weight-medium">
-                      4
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-                    <td>
-
-                    </td>
-                  </tr>
                 </tbody>
               </table>
+
             </div>
             <br></br>
-
-            <div class="row">
-              <div class="col-sm-5">
-                <div class="dataTables_info" id="kelas_info" role="status" aria-live="polite">Showing 1 to 10 of 743 entries
-                </div>
-              </div>
-
-              <div class="col-sm-7">
-
-                  <ul class="tombol-tabel">
-                    <a href="#" class="previous">&laquo; Previous</a>
-                    <a href="#" class="next">Next &raquo;</a>
-                  </ul>
-
-              </div>
-            </div>
 
           </div>
         </div>
@@ -216,3 +97,67 @@
 
     </div>
 <!-- main-panel ends -->
+
+<!-- Buat DataTable-->
+<script type="text/javascript" src="<?php echo base_url() . 'assets/js/jquery-3.2.1.js' ?>"></script>
+<script type="text/javascript" src="<?php echo base_url() . 'assets/js/jquery.dataTables.js' ?>"></script>
+<script>
+  $(document).ready(function() {
+    show_konsul();
+    $('#tabelKonsul').dataTable();
+
+    //function show all product
+    function show_konsul() {
+      $.ajax({
+        type: 'ajax',
+        url: '<?php echo site_url('konsultasimahasiswa/konsul_data') ?>',
+        async: false,
+        dataType: 'json',
+        success: function(data) {
+          var html = '';
+          var i;
+          for (i = 0; i < data.length; i++) {
+            var no = i+1;
+            html += '<tr>' +
+              '<td>' + no + '</td>' +
+              '<td>' + data[i].Tanggal + '</td>' +
+              '<td>' + data[i].Status + '</td>' +
+              '<td>' + data[i].File + '</td>' +
+              '<td>' + data[i].Keterangan + '</td>' +
+              '<td>' + data[i].Komentar + '</td>' +
+              '</tr>';
+          }
+          $('#show_data').html(html);
+        }
+
+      });
+    }
+
+    $('#submit').submit(function(e){
+          e.preventDefault();
+          console.log(new FormData(this));
+               $.ajax({
+                   url:'<?php echo base_url();?>konsultasimahasiswa/insert_data',
+                   type:"post",
+                   data:new FormData(this),
+                   processData:false,
+                   contentType:false,
+                   cache:false,
+                   async:false,
+                  success: function(data){
+                        show_konsul();
+                 },
+                 error: function(jqXHR, textStatus, errorThrown) {
+                  console.log(textStatus, errorThrown);
+                }
+               });
+          });
+
+    $('#dosen').change(function() {
+
+      $("#nip").val($("#dosen").val());
+    });
+
+  });
+
+  </script>
