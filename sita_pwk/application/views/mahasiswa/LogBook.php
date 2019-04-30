2e349fcdb535c7b1ee8dd56569e74bd4a90fa81e
Febri Dwi Putro <febridwipoethro@gmail.com>
2019-04-30 12:05:55 +0700

    edit logbook mahasiswa

--- sita_pwk/application/views/mahasiswa/LogBook.php
+++ sita_pwk/application/views/mahasiswa/LogBook.php
@@ -102,7 +102,7 @@
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Edit Logbook</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Edit Logbook aja</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
