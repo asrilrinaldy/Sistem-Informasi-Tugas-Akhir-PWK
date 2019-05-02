bc2d6608ba63424e5e91fd758eb799f695cc5ed5
Asril Rinaldi <asrilrinaldi@gmail.com>
2019-05-02 22:09:36 +0700

    halaman lupa sandi

---
+++ sita_pwk/application/controllers/lupaKataSandi.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$isi['konten'] = '';
+		$this->load->view('', $isi);
+	}
+}
