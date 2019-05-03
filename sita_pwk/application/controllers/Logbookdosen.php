44b902b2aa776f0679d98189a4b2aac33ac8e7d3
mhdfahmi <44706387+mhdfahmi@users.noreply.github.com>
2019-05-03 08:52:58 +0700

    Update Logbookdosen.php

--- sita_pwk/application/controllers/Logbookdosen.php
+++ sita_pwk/application/controllers/Logbookdosen.php
@@ -8,4 +8,28 @@ class Logbookdosen extends CI_Controller {
 		$isi['konten'] = 'dosen/logbookdosen';
 		$this->load->view('dosen/template', $isi);
 	}
+
+	public function mahasiswa_data(){
+		$this->load->model('mahasiswa_model');
+		$nip = $this->session->userdata('ses_id');
+		$data = $this->mahasiswa_model->get_mhs_nip($nip);
+		echo json_encode($data);
+	}
+
+	public function detail_logbook(){
+		$this->load->model('mahasiswa_model');
+		$mahasiswa = $this->mahasiswa_model->get_mhs_nim($this->input->get('nim'));
+		$isi['mahasiswa'] = $mahasiswa->row_array();
+		$isi['konten'] = 'dosen/detail_logbook';
+		$this->load->view('dosen/template', $isi);
+	}
+
+	function logbook_data_mhs()
+	{
+		$nim = $this->input->get('nim');
+		$this->load->model('mahasiswa_model');
+		$data = $this->mahasiswa_model->logbook_mahasiswa_by_NIM($nim);
+		echo json_encode($data);
+	}
+
 }
